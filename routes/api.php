bc1c9ed0abba31610a48ad58beae3f4900372136
Oswin Jerome <oswinjeromej@gmail.com>
2020-04-24 19:05:50 +0530

    Bug fixes

--- routes/api.php
+++ routes/api.php
@@ -28,18 +28,6 @@ Route::get('/books',function(Request $request){
     }
     return $b;
 });
-// Route::get('/',function(Request $request){
-    
-//     $to_name = 'User';
-//     $to_email = 'oswinsherwin@gmail.com';
-//     $data = array('name'=>"Ogbonna Vitalis(sender_name)", "body" => "A test mail");
-//     Mail::send('emails.user', $data, function($message) use ($to_name, $to_email) {
-//     $message->to($to_email, $to_name)
-//     ->subject('Laravel Test Mail');
-//     $message->from('oswinjeromej@gmail.com','Test Mail');
-//     });
-
-// });
 
 
 
